c3d80c552f198290131628445c24d4f1c087ad43
Pedro Henrique Ramos Martins <pedrohrmartins98@gmail.com>
2021-07-25 15:23:49 -0300

    Aprendendo a utilizar o Enum
    Aprendendo a converter valores do tipo string e inteiros em Enum e retornando o valor no terminal do windows.
    
    Criando o Enum OrderStatus.
    Criando a classe ordem.

---
+++ Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningEnum.Entities.Enums;
+
+namespace LearningEnum.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return 
+                "ID: " + this.Id + 
+                ", Data: " + this.Moment + 
+                ", Status: " + this.Status;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearningEnum.Entities;
+using LearningEnum.Entities.Enums;
 
 namespace LearningEnum
 {
@@ -10,6 +12,29 @@ namespace LearningEnum
     {
         static void Main(string[] args)
         {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendinPayment,
+            };
+            
+
+            // Forma para fazer a conversão de inteiro para Enum
+            string teste = OrderStatus.Processing.ToString();
+            
+            // 1 Forma de fazer a conversão de string para inteiro
+            OrderStatus teste2 = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Processing");
+
+            // 2 Forma de fazer a conversão de string para Enum
+            OrderStatus os;
+            Enum.TryParse("Processing", out os);
+
+
+            Console.WriteLine(order);
+            Console.WriteLine(teste);
+            Console.WriteLine(os);
+            Console.WriteLine(teste2);
         }
     }
 }
